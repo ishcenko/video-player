14b10935af6e3e9ff8bc61d8f5ca24c7e4fe94c0
ishcenko <a_ishcenko@i.ua>
2024-03-05 12:19:05 +0200

    не працює відео

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { Player } from 'components/Player/Player';
+import { GlobalStyle } from 'GlobalStyled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <Player />
+    </GlobalStyle>
   </React.StrictMode>
 );
